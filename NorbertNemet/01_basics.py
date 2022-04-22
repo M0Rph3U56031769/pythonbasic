9c55a585f079fa57870a348f4381a0131d3c9a4f
NNemet <nemet.norbi.1@gmail.com>
2022-04-22 13:50:02 +0200

    HF1

--- NorbertNemet/01_basics.py
+++ NorbertNemet/01_basics.py
@@ -1,2 +1,20 @@
-name: str = input("What is you name? ")  # Input your name
-print(f"Hi {name}!")  # Print a welcome message
+import datetime  # import datetime module
+
+
+def addName() -> str:
+    name: str = input(f"{datetime.datetime.now()} - What is you name? ")  # Input your name
+    return name
+
+
+def welcome(name):
+    if name != "":
+        print(f"{datetime.datetime.now()} - Welcome {name}!")
+    else:
+        print(f"{datetime.datetime.now()} - Please add your name!")
+        name = addName()
+        welcome(name)
+
+
+yourName = addName()
+welcome(yourName)
+
