5176b53276b7b934f420a468ef066c0c0ad3fda9
Nagy Daniel <daniel.nagy@dmgmori-heitec.hu>
2022-04-26 14:45:41 +0200

    2. alkalom

---
+++ 02_ciklusok_adatok.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import datetime
+import time
+from pprint import pprint
+
+
+def dict_test():
+    dict_a_thor = {
+        "window": 78000,
+        "door": 60000,
+        "boolean_var": {
+            "sub": "asdasd"
+        }
+    }
+
+    dict_a_thor.pop("door")
+    print("#"*50)
+    print(dict_a_thor)
+    pprint(dict_a_thor)
+    dict_a_thor["window"] = 80000
+    print(f"How much is the window: {dict_a_thor.get('window')}")
+    print(f"How much is the window: {dict_a_thor['window']}")
+
+
+def list_and_for_cycle():
+    test_list = [
+        "e1",
+        45,
+        123.23,
+        [
+            "e2",
+            "e3"
+        ]
+    ]
+
+    print(range(5, 10))
+    print(len(test_list))
+
+    test_list.append("Appended data")
+
+    for index in range(len(test_list)):
+        print(f"{index}: {test_list[index]}")
+
+    print("\n")
+    for index, ELEM in enumerate(test_list):
+        if type(ELEM) == list:
+            print(f"This is a list:")
+            for i, e in enumerate(ELEM):
+                print(f"\t{i}: {e}")
+            print("-"*50)
+        else:
+            print(f"{index} - {ELEM}")
+
+
+def while_true():
+    while True:
+        timestamp = datetime.datetime.now()
+        print(timestamp)
+
+        time.sleep(1)
+        if str(9) in str(timestamp):
+            break
+    return
+
+
+# dict_test()
+# list_and_for_cycle()
+
+while_true()
---
+++ 03_classes.py
@@ -0,0 +1,44 @@
+# Author: DNAGY
+
+"""
+Module is not for import.
+"""
+
+
+class MainClass:
+    """
+    This class is for practice.
+    """
+
+    class_variable: str = "default value"
+
+    def __init__(self, class_variable: str = "Default Argument"):
+        self.class_variable = class_variable
+
+    def just_a_method(self):
+        print(self.class_variable)
+
+    @staticmethod
+    def static_thing():
+        """
+        this method type will try to reach a class variable without success.
+        :param:
+        :return:
+        """
+
+        try:
+            print(self.class_variable)
+        except Exception as err:
+            print(err)
+
+        print("This is a static method. cannot use class variables")
+
+
+if __name__ == "__main__":
+    instance = MainClass("NonDEF value")
+    instance.just_a_method()
+    instance.static_thing()
+
+    instance2 = MainClass()
+    instance2.just_a_method()
+    instance2.static_thing()
