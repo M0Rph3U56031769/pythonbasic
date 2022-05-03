e507d1909439289137e764a6a20d5afead442dad
NNemet <nemet.norbi.1@gmail.com>
2022-05-03 16:44:30 +0200

    HF2 - updated

--- NorbertNemet/02_HF.py
+++ NorbertNemet/02_HF.py
@@ -1,37 +1,37 @@
 import time
 
-productsDict = {
+products_dict = {
     "door": 30000,
     "window": 67000
 }
 
 
-def print_prod(products):
+def print_prod(products: dict):
     print(f"Product list:")
     for index, (element, price) in enumerate(products.items()):
         print(f"\t{index}: {element}: {price}")  # \t - tabulator"""
 
 
-def add(element, price):
-    productsDict[element] = price  # Modify/add
+def add(element: str, price: int):
+    products_dict[element] = price  # Modify/add
     print("Element is added/modified!")
 
 
-def delete(element, products):
+def delete(element: str, products: dict):
     if element in products:
-        productsDict.pop(element)  # Remove element
+        products_dict.pop(element)  # Remove element
         print("Element is deleted!")
     else:
         print("The typed name doesn't exist!")
 
 
-def repo(products):  # Infinite loop
+def repo(products: dict):  # Infinite loop
     while True:
         print("\n" + "-" * 25 + " Repository program " + "-" * 25)
         print_prod(products)
-        option = ""
-        element = ""
-        price = ""
+        option: str = str()
+        element: str = str()
+        price: int = int()
         print("\nAvailable options: \n A - Add/Modify \n B - Delete \n C - Escape")
         option = input("\nSelect an option: ")
         if option == "A":
@@ -51,4 +51,4 @@ def repo(products):  # Infinite loop
         time.sleep(1)
 
 
-repo(productsDict)
+repo(products_dict)
