0aacef74cbc0166a2e4a26edb5a9e2f287c9e44c
vcsok <Viktor.Csok@dmgmori-heitec.hu>
2022-05-10 12:42:14 +0200

    Inventory handler backend

---
+++ vcsokwarehouse/modules/menuhandler.py
@@ -0,0 +1,19 @@
+from datahandler import DataHandler
+
+
+class MenuHandler:
+
+    def __init__(self):
+        self.data_handler = DataHandler()
+
+    def start_menu(self):
+        pass
+
+    def main_menu(self):
+        pass
+
+    def add_update_menu(self):
+        pass
+
+    def remove_menu(self):
+        pass
