fad7d7aa9e1b0c4c726d3e4fa0faccb64a110ee0
M0Rph3U56031769 <nagydaniel1337@gmail.com>
2022-05-19 07:39:34 +0200

    pylint fix

--- warehouse/modules/export.py
+++ warehouse/modules/export.py
@@ -2,14 +2,13 @@ import json
 
 import pandas as pd
 
-
 with open(file="data.json", mode="r", encoding="utf-8") as json_file:
     data = json.load(json_file)
 
+print(f"{'Key': <8} {'Label':<15} {'Number':<10}")
 for k, v in data.get("database").items():
     label, num = v
-    print("{:<8} {:<15} {:<10}".format(k, label, num))
-
+    print(f"{k:<8} {label:<15} {num:<10}")
 
 df = pd.read_json(r'data.json')
 df.to_csv(r'data.csv', index=None, header=False)
